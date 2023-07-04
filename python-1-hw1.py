ee8b9907f217e422760385bd7f46e78f42256aff
a.rubtsov <rubtsovandrii@gmail.com>
2023-07-04 20:33:00 +0100

    added operation function

--- python-1-hw1.py
+++ python-1-hw1.py
@@ -1,19 +1,45 @@
 #!/usr/bin/env python3
-
+import operator
 
 #Define function to accept user input 
 
 def get_user_input(sequence_number):
     while True: 
         try:
-            user_input = input(f'Please enter {sequence_number} number: ')   
+            user_input = input(f'Please enter the {sequence_number} number: ')   
             user_input = user_input.replace(',', '.')
             return float(user_input)
         except ValueError: 
             print("ValueError, try again")
-#        finally:
-#            print("Success!")
+
+def get_user_operation():
+    operations = {
+        1: operator.add,
+        2: operator.sub,
+        3: operator.mul,
+        4: operator.truediv
+    }
+    while True:
+        try: 
+            user_input = int(input("""Please select an operation: 
+            1. Addition
+            2. Subtraction
+            3. Multiplication
+            4. Division
+            Enter your choice (1-4): """))
+            if user_input in operations.keys():
+                return operations[user_input]
+            else:
+                print("Invalid choice, please enter a valid operation!")
+        except ValueError:
+            print("Invalid input, please enter a number!")
+
+
+
 
 #For debug
-print(get_user_input("first"))
-print(get_user_input("second"))
+number1 = get_user_input("first")
+number2 = get_user_input("second")
+choice = get_user_operation()
+result = choice(number1, number2)
+print(result)
